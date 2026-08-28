9097e307abaa2fc99a9ec556e75069db02aea9d5
Zebi <einz.yume@gmail.com>
2026-08-28 12:45:18 +0900

    [Feat] trying design Cart
    쇼핑카트 구현중

---
+++ 2608test/2608test/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace _2608test;
+
+public class ShoppingCart<T> where T : Goods
+{
+    List<Goods> Cart = new List<Goods>();
+
+
+    public void Add(T goods)
+    {
+        Cart.Add(goods);
+    }
+
+    public void Remove(T goods)
+    {
+        if (Cart.Contains(goods))
+        {
+            Cart.Remove(goods);
+        }
+        else
+        {
+            Console.WriteLine("장바구니에 해당 상품이 없습니다");
+        }
+
+    }
+}
