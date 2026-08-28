ba2d56030b0fa263d560d998c0dd2566c51c1864
Zebi <einz.yume@gmail.com>
2026-08-28 12:30:44 +0900

    [Feat] Snack Class
    Snack Class 구현

--- 2608test/2608test/Drinks.cs
+++ 2608test/2608test/Drinks.cs
@@ -12,7 +12,7 @@ public class Drinks : Foods
 
     public DrinkList Drink {get; protected set;}
     public bool IsThisSale {get; protected set;}
-    private const int SALE_RATE = 30;
+    private const int DRINK_SALE = 30;
     
     
     public Drinks(string name, int baseprice, DrinkList drink) : base(name, baseprice) 
@@ -21,13 +21,15 @@ public class Drinks : Foods
         if (drink == DrinkList.Coke
             || drink == DrinkList.Cider) // 이거 서로 다른 열거형에서 이름 일치하는지 비교 하는법 없나?
             IsThisSale = true;
+        else 
+            IsThisSale = false;
     }
 
     public override void CalPrice()
     {
         if (IsThisSale)
         {
-            Price = (BasePrice * (100-SALE_RATE))/100;
+            Price = (BasePrice * (100-DRINK_SALE))/100;
         }
         else
         {
--- 2608test/2608test/Snacks.cs
+++ 2608test/2608test/Snacks.cs
@@ -9,4 +9,30 @@ public class Snacks : Foods
         Candy,
         Cookie
     }
+    public SnackList Snack {get; protected set;}
+    public bool IsThisSale {get; protected set;}
+    private const int SNACK_SALE = 20;
+    
+    
+    public Snacks(string name, int baseprice, SnackList snack) : base(name, baseprice) 
+    {
+        Snack = snack;
+        if (snack == SnackList.Chips
+            || snack == SnackList.Chocolate) // 이거 서로 다른 열거형에서 이름 일치하는지 비교 하는법 없나?
+            IsThisSale = true;
+        else 
+            IsThisSale = false;
+    }
+
+    public override void CalPrice()
+    {
+        if (IsThisSale)
+        {
+            Price = (BasePrice * (100-SNACK_SALE))/100;
+        }
+        else
+        {
+            base.CalPrice();
+        }
+    }
 }
