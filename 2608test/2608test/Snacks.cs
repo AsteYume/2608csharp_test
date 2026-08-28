c8e4b3b9e3de8acfee5a2cd52b9a4bb22673e473
Zebi <einz.yume@gmail.com>
2026-08-28 10:42:34 +0900

    [Feat] Add Snack Fix Drink
    Snack 같은 방식으로 추가
    Snack Drinks에 None 상태 제거

--- 2608test/2608test/Snacks.cs
+++ 2608test/2608test/Snacks.cs
@@ -1,6 +1,19 @@
 ﻿namespace _2608test;
 
-public class Snacks
+public enum SnackList
 {
+    Chips,
+    Cookie,
+    Chocolate,
+    Candy
+}
+
+public class Snacks : Foods
+{
+    public SnackList Snack { get; protected set; }
     
+    public Snacks(string name, int price, SnackList snack) : base(name, price)
+    {
+        Snack = snack;
+    }
 }
